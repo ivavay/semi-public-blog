58fc6bd0cdbe919f5eeaf4bed7044d0ee0c7a9c7
Ivy Chen <ichen9@ucsc.edu>
2021-08-16 16:09:49 -0700

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('images')
+    eleventyConfig.addPassthroughCopy('admin')
+    eleventyConfig.addPassthroughCopy('css')
+    // return {
+    //   passthroughFileCopy: true
+    // }
+  
+    const {
+      DateTime
+    } = require("luxon");
+  
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+      eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, {
+          zone: 'utc'
+        }).toFormat('yy-MM-dd');
+      });
+  
+      eleventyConfig.addFilter("readableDate", dateObj => {
+      return DateTime.fromJSDate(dateObj, {
+        zone: 'utc'
+      }).toFormat("MMMM dd, yyyy");
+    });
+  
+  };
+  
---
+++ posts/my-first-post.md
@@ -0,0 +1,7 @@
+---
+layout: layouts/post.njk
+title: My first post
+description: The first post on the Eleventy + Netlify CMS from scratch blog
+date: 2020-04-18
+---
+Hello, here is the body of the post.
